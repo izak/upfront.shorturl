6bfcc73d39d3d01f9fb687e0d1b5000406751e94
Izak Burger <isburger@gmail.com>
2011-12-22 15:40:32 +0200

    Skeleton of product

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name='upfront.shorturl',
+    version=version,
+    description="Provide an auto-complete Change Note field for CMFEditions",
+    long_description=open("README.txt").read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        ],
+    keywords='url shortener redirector',
+    author='Izak Burger, Upfront Systems',
+    author_email='isburger@gmail.com',
+    url='https://github.com/izak/upfront.shorturl',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir = {'' : 'src'},
+    namespace_packages=['upfront'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFPlone',
+    ],
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+    """,
+    setup_requires=["PasteScript"],
+    paster_plugins = ["ZopeSkel"],
+    )
