1b307a25df0ca2f481c35cb6e35815e99cadf07e
Izak Burger <isburger@gmail.com>
2011-12-22 15:49:50 +0200

    first version of storage, intested

---
+++ src/upfront/shorturl/interfaces.py
@@ -0,0 +1,17 @@
+from zope.interface import Interface, Attribute
+
+
+class IShortURLStorage(Interface):
+    """A storage for items where the old and the new location are known.
+
+    Will be registered as a local utility.
+    """
+
+    def add(short, target):
+        """Add a short code and the target to redirect to. """
+
+    def remove(short):
+        """Remove a short code, and its target. """
+
+    def get(short, default=None):
+        """ Return the target for the given short, or default if not found. """
---
+++ src/upfront/shorturl/storage.py
@@ -0,0 +1,24 @@
+from zope.interface import implements
+
+from persistent import Persistent
+from BTrees.OOBTree import OOBTree, OOSet
+
+from upfront.shorturl.interfaces import IShortURLStorage
+
+
+class ShortURLStorage(Persistent):
+    """Stores short codes and urls to redirect to. """
+    implements(IShortURLStorage)
+
+    def __init__(self):
+        self._map = OOBTree()
+
+    def add(self, short, target):
+        self._map[short] = target
+
+    def remove(self, short):
+        if self._map.has_key(short):
+            del self._map[short]
+
+    def get(self, short, default=None):
+        return self._map.get(short, default)
